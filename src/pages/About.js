408475cb01b151b03c51f64e68c788cbbc6c9d52
delbin <imdelbingeorge@gmail.com>
2022-10-04 00:27:19 +0530

    small changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,10 +13,10 @@ function About() {
                     <div className="about-section-content">
                         <h1 className="section-heading">Designer, Frontend Developer & Self learner</h1>
                         <p>
-                            Hey, I'm probably not the typical designer positioned behind an Illustrator art board adjusting
-                            pixels, but I design. I'm an entry-level front-end developer who loves to build and maintain web
-                            pages. Seeking for new opportunities and challenges that will expand my skill set. I am currently
-                            doing BCA.
+                            My name is Delbin George I'm from India, I'm probably not the typical designer positioned behind
+                            an Illustrator art board adjusting pixels, but I design. I'm an entry-level front-end developer
+                            who loves to build and maintain web pages designs stylish user interfaces that inspires!. Seeking
+                            for new opportunities and challenges that will expand my skill set. I am currently doing BCA.
                         </p>
                     </div>
                 </div>
