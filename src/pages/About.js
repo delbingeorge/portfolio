4ad59ac5c21c52be565461a0671fe26d18e9866b
delbin <imdelbingeorge@gmail.com>
2022-10-04 11:06:56 +0530

    completely removed react reveal

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,24 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
 
 import proImg from "../media/images/proImg.jpg";
 function About() {
     return (
         <div id="aboutme">
             <section className="about-section">
-                <Fade bottom cascade duration={2940} delay={50} damping={1} >
-                    <div className="about-div">
-                        <div className="about-section-img">
-                            <img src={proImg} alt="Delbin George Portfolio "></img>
-                        </div>
-                        <div className="about-section-content">
-                            <h1 className="section-heading">Designer, Frontend Developer & Self learner</h1>
-                            <p>
-                                My name is Delbin George I'm from India, I'm probably not the typical designer positioned
-                                behind an Illustrator art board adjusting pixels, but I design. I'm an entry-level front-end
-                                developer who loves to build and maintain web pages designs stylish user interfaces that
-                                inspires!. Seeking for new opportunities and challenges that will expand my skill set. I am
-                                currently doing BCA.
-                            </p>
-                        </div>
+                <div className="about-div">
+                    <div className="about-section-img">
+                        <img src={proImg} alt="Delbin George Portfolio "></img>
                     </div>
-                </Fade>
+                    <div className="about-section-content">
+                        <h1 className="section-heading">Designer, Frontend Developer & Self learner</h1>
+                        <p>
+                            My name is Delbin George I'm from India, I'm probably not the typical designer positioned behind
+                            an Illustrator art board adjusting pixels, but I design. I'm an entry-level front-end developer
+                            who loves to build and maintain web pages designs stylish user interfaces that inspires!. Seeking
+                            for new opportunities and challenges that will expand my skill set. I am currently doing BCA.
+                        </p>
+                    </div>
+                </div>
             </section>
         </div>
     );
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,23 +1,20 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
 function Skills() {
     return (
         <div>
             <section className="my-skills">
-                <Fade bottom cascade duration={2940} delay={50} damping={1}>
-                    <div className="skill-div-main">
-                        <h1 className="section-heading">My Skills & Knowledge</h1>
-                        <div className="skill-div">
-                            <div className="skill-div-items">HTML</div>
-                            <div className="skill-div-items">CSS</div>
-                            {/* <div className="skill-div-items"></div> */}
-                            <div className="skill-div-items">JavaScript</div>
-                            <div className="skill-div-items">React JS</div>
-                            <div className="skill-div-items">Git </div>
-                            {/* <div className="skill-div-items">Creative Cloud</div> */}
-                        </div>
+                <div className="skill-div-main">
+                    <h1 className="section-heading">My Skills & Knowledge</h1>
+                    <div className="skill-div">
+                        <div className="skill-div-items">HTML</div>
+                        <div className="skill-div-items">CSS</div>
+                        {/* <div className="skill-div-items"></div> */}
+                        <div className="skill-div-items">JavaScript</div>
+                        <div className="skill-div-items">React JS</div>
+                        <div className="skill-div-items">Git </div>
+                        {/* <div className="skill-div-items">Creative Cloud</div> */}
                     </div>
-                </Fade>
+                </div>
             </section>
         </div>
     );
