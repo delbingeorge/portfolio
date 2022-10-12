6aabadb49d4d13def71930a3a8dac8b8fb1a6115
delbin <imdelbingeorge@gmail.com>
2022-10-12 23:14:35 +0530

    changes

--- src/pages/Awards/Awards.js
+++ src/pages/Awards/Awards.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../Awards/Awards.css";
 
-import medalImg from "../../media/images/medal.png";
-import { Link } from "react-scroll";
+// import medalImg from "../../media/images/medal.png";
+// import { Link } from "react-scroll";
 
 function Achieve() {
     return (
@@ -11,7 +11,7 @@ function Achieve() {
                 <div className="awards-div-main">
                     <h1 className="section-heading">Achievements</h1>
                     <div className="awards-div">
-                        <Link to="firstcertificate" spy={true} smooth={true} offset={10} duration={1950} className="award">
+                        {/* <Link to="firstcertificate" spy={true} smooth={true} offset={10} duration={1950} className="award">
                             <img alt="" src={medalImg}></img>
                             <h1>
                                 Web Designing <br></br>Competition
@@ -34,7 +34,7 @@ function Achieve() {
                             </h1>
                             <h2>[ Inter Class ]</h2>
                             <p>Champion</p>
-                        </Link>
+                        </Link> */}
                     </div>
                 </div>
             </section>
