b266d5e6282305bc3e0d462b9b76c3c5121227d6
delbin <imdelbingeorge@gmail.com>
2022-10-13 20:41:47 +0530

    finally final push final one

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -7,6 +7,17 @@ function Gallery() {
             <section className="gallery-section">
                 <h1 className="section-heading">Achievements</h1>
                 <div className="gallery-div">
+                    <div className="gallery" id="thirdcertificate">
+                        <div className="gallery-img img-three"></div>
+                        <div className="gallery-content">
+                            <h1 className="gallery-content-head">St. Aloysius College (Autonomous)</h1>
+                            <h2 className="gallery-award-title">CHAMPION</h2>
+                            <h3 className="gallery-award-cate">[inter class]</h3>
+                            <p className="gallery-content-para">
+                                TECHNOPHILIA, IT Fest conducted by BCA Department on 21th April 2022.
+                            </p>
+                        </div>
+                    </div>
                     <div className="gallery" id="firstcertificate">
                         <div className="gallery-img "></div>
                         <div className="gallery-content">
@@ -28,17 +39,6 @@ function Gallery() {
                             <h3 className="gallery-award-cate">[inter College]</h3>
                             <p className="gallery-content-para">
                                 Joshiana IT Fest by MCA Department of St.Joseph on 7th Sept 2022.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="gallery" id="thirdcertificate">
-                        <div className="gallery-img img-three"></div>
-                        <div className="gallery-content">
-                            <h1 className="gallery-content-head">St. Aloysius College (Autonomous)</h1>
-                            <h2 className="gallery-award-title">CHAMPION</h2>
-                            <h3 className="gallery-award-cate">[inter class]</h3>
-                            <p className="gallery-content-para">
-                                TECHNOPHILIA, IT Fest conducted by BCA Department on 21th April 2022.
                             </p>
                         </div>
                     </div>
