b15e2a463b3d55d7a31f2ae41141b81d76ffaeb2
Delbin George <imdelbingeorge@gmail.com>
2023-11-30 20:11:11 +0530

    added vercel analytics

---
+++ src/IIndex.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IIndex() {
+     return (
+          <div>
+               <h1>Oops, something went wrong!</h1>
+          </div>
+     )
+}
+
+export default IIndex
