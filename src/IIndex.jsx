d0c5e4340b1bb4d19beb05a53821072ff8714d1b
Delbin George <imdelbingeorge@gmail.com>
2024-02-02 19:32:28 +0530

    some path changed!

--- src/IIndex.jsx
+++ src/IIndex.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function IIndex() {
      return (
           <div className='min-h-screen flex flex-col justify-center items-center'>
-               <img src="public/error.svg" className='h-[46vh]' alt="" />
+               <img src="/public/error.svg" className='h-[46vh]' alt="" />
                <h1 className='font-semibold font-mono-space'>Oops, something went wrong!</h1>
           </div>
      )
